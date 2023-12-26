4ea79336667a4f268394167d180f5a972ea3266e
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-12-25 21:07:18 -0500

    feat(LinAlgNumber) : implemented the PartialEq<LinAlgNumber> trait.
    Also removed the None variant from the LinAlgNumber enum since it seemed to overlap with NaN.

--- src/linear_algebra_number/mod.rs
+++ src/linear_algebra_number/mod.rs
@@ -1,10 +1,10 @@
+use std::cmp::Ordering::{Less, Equal, Greater};
 
-#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, PartialOrd, Ord)]
 pub enum LinAlgNumber{
     Float64(f64),
     Float32(f32),
     NaN,
-    None
 }
 //From Gauntlet
 impl From<f64> for LinAlgNumber{
@@ -25,4 +25,40 @@ impl From<i32> for LinAlgNumber{
     }
 }
 //Impl Comparison traits
+impl Eq for LinAlgNumber{}
+//Partial Eqs
+impl PartialEq<LinAlgNumber> for LinAlgNumber{
+    fn eq(&self, other: &Self) -> bool {
+       let comparison = match self{
+        LinAlgNumber::Float64(value_self) => {
+            match other{
+                LinAlgNumber::Float64(value_other) => value_self.partial_cmp(value_other).unwrap_or(Less)
+                ,
+                LinAlgNumber::Float32(value_other) => value_self.partial_cmp(&(*value_other as f64)).unwrap_or(Less),
+                LinAlgNumber::NaN => Less,
+            }
+        },
+        LinAlgNumber::Float32(value_self) => {
+            match other{
+                LinAlgNumber::Float64(value_other) => (*value_self as f64).partial_cmp(value_other).unwrap_or(Less)
+                ,
+                LinAlgNumber::Float32(value_other) => value_self.partial_cmp(value_other).unwrap_or(Less),
+                LinAlgNumber::NaN => Less,
+            }
+        },
+        LinAlgNumber::NaN => Less,
+    };
+    comparison == Equal
+}}
+
+impl PartialEq<f64> for LinAlgNumber{
+
+}
+
+impl PartialEq<f32> for LinAlgNumber{
+
+}
+
+
+
 //TODO: WRITE THE COMPARISON LOGIC
